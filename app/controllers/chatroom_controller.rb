8cd9e21a8bdeaa9745d47df043de915ac5021bbc
hanskristian7 <hans.kristian1509@gmail.com>
2020-06-06 15:59:45 +0700

    session is established

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,10 +1,12 @@
 class ChatroomController < ApplicationController
+    before_action :require_user
+    
     def index
         @messages = Message.all
     end
 end
 
-private
-def set_message
+# private
+# def set_message
 
-end
+# end
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,11 +3,21 @@ class SessionController < ApplicationController
         
     end
 
-    def show
-        @user = User.find_by(params[:id])
+    def create
+        user = User.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:success] = "Login Successfully!"
+            redirect_to chatroom_path
+        else
+            flash.now[:error] =  "Login Failed!, Please Try Again."
+            render 'new'
+        end
     end
 
-    def create
-        
+    def destroy
+        session[:user_id] = nil
+        flash[:success] = "Logged Out!"
+        redirect_to root_path
     end
 end
