c2cd12d9a79a66afefc922add4f898ba1a3b7ad1
hanskristian7 <hans.kristian1509@gmail.com>
2020-06-06 16:45:11 +0700

    send messages done

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get 'login', to: 'session#new'
   post 'login', to: 'session#create'
   delete 'logout', to: 'session#destroy'
+
+  post 'message', to: 'message#create'
 end
